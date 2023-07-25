6390b4eb415cd98c1f067966aec9c2b1dbcfe608
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2023-07-25 22:21:59 +0100

    class(181)

--- 08-food-order-app/src/store/cart-context.js
+++ 08-food-order-app/src/store/cart-context.js
@@ -4,7 +4,7 @@ const CartContext = createContext({
   items: [],
   totalAmount: 0,
   addItem: (item) => {},
-  remoteItem: (id) => {},
+  removeItem: (id) => {},
 });
 
 export default CartContext;
