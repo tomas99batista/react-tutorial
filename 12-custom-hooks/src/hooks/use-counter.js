16f42f0aab48fd4cb7f9dca52a1b32f9e4b5fb6c
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2023-07-26 19:42:40 +0100

    class(223)

--- 12-custom-hooks/src/hooks/use-counter.js
+++ 12-custom-hooks/src/hooks/use-counter.js
@@ -1,3 +1,21 @@
-const useCounter = (props) => {};
+import { useState, useEffect } from "react";
+
+const useCounter = (forwards = true) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forwards]);
+
+  return counter;
+};
 
 export default useCounter;
