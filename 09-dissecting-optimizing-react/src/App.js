c1b0faf2cf0e0884c90f199bd8c3a7768d3ce583
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2023-07-26 13:17:13 +0100

    class(185)

---
+++ 09-dissecting-optimizing-react/src/App.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+import Button from "./components/UI/Button/Button";
+
+function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  };
+  return (
+    <div className="app">
+      <h1>Hi there!</h1>
+      {showParagraph && <p>This is new!</p>}
+      <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
+    </div>
+  );
+}
+
+export default App;
