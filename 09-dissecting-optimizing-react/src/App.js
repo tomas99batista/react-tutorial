92489e1a460d80593d9c3a3094c23a4b88c7aa23
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2023-07-26 17:21:10 +0100

    class(190)

--- 09-dissecting-optimizing-react/src/App.js
+++ 09-dissecting-optimizing-react/src/App.js
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import "./App.css";
+import DemoOutput from "./components/Demo/DemoOutput";
 import Button from "./components/UI/Button/Button";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+  console.log("APP RUNNING");
+
+  const toggleParagraphHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowParagraph((prevShowParagraph) => !prevShowParagraph);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      {showParagraph && <p>This is new!</p>}
-      <Button onClick={toggleParagraphHandler}>Toggle paragraph</Button>
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
+      <Button onClick={toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # react-tutorial
 https://www.udemy.com/course/react-the-complete-guide-incl-redux/
+https://github.com/academind/react-complete-guide-code/tree/master
