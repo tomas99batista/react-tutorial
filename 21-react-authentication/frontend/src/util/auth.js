6c51b185097e4060daf5dde9fbd921eb35327e4f
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2023-11-02 23:06:57 +0000

    class(498)

--- 21-react-authentication/frontend/src/util/auth.js
+++ 21-react-authentication/frontend/src/util/auth.js
@@ -1,3 +1,19 @@
+import { redirect } from "react-router-dom";
+
 export const getAuthToken = () => {
   return localStorage.getItem("token");
 };
+
+export function tokenLoader() {
+  return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) {
+    return redirect("/auth");
+  }
+
+  return null;
+}
