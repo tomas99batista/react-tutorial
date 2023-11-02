5e39967977f5266710f7aa26a90d27111ac7bb9e
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2023-11-02 23:10:49 +0000

    class(498)

--- 21-react-authentication/frontend/src/util/auth.js
+++ 21-react-authentication/frontend/src/util/auth.js
@@ -1,11 +1,32 @@
 import { redirect } from "react-router-dom";
 
-export const getAuthToken = () => {
-  return localStorage.getItem("token");
-};
+export function getTokenDuration() {
+  const storedExpirationDate = localStorage.getItem("expiration");
+  const expirationDate = new Date(storedExpirationDate);
+  const now = new Date();
+  const duration = expirationDate.getTime() - now.getTime();
+  return duration;
+}
+
+export function getAuthToken() {
+  const token = localStorage.getItem("token");
+
+  if (!token) {
+    return null;
+  }
+
+  const tokenDuration = getTokenDuration();
+
+  if (tokenDuration < 0) {
+    return "EXPIRED";
+  }
+
+  return token;
+}
 
 export function tokenLoader() {
-  return getAuthToken();
+  const token = getAuthToken();
+  return token;
 }
 
 export function checkAuthLoader() {
@@ -14,6 +35,4 @@ export function checkAuthLoader() {
   if (!token) {
     return redirect("/auth");
   }
-
-  return null;
 }
