502f7e60eb3e6b9b55c841e16c51864eaf475392
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2023-11-08 20:41:23 +0000

    class(591)

--- 27-testing-react/src/components/Greeting.test.js
+++ 27-testing-react/src/components/Greeting.test.js
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 
 describe("<Greeting />", () => {
@@ -12,5 +13,31 @@ describe("<Greeting />", () => {
     // Assert
     const helloWorldElement = screen.getByText("Hello World", { exact: false });
     expect(helloWorldElement).toBeInTheDocument();
+  });
+
+  test("should render 'good to see you' if button was NOT clicked", () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("should render 'changed' if button WAS clicked", () => {
+    render(<Greeting />);
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test("should not render 'good to see you' if button WAS clicked", () => {
+    render(<Greeting />);
+    const buttonElement = screen.getByRole("button");
+    userEvent.click(buttonElement);
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
   });
 });
