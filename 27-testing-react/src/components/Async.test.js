33525a9d59d87e10ea54b861719e5c914810592d
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2023-11-08 20:55:29 +0000

    class(593)

--- 27-testing-react/src/components/Async.test.js
+++ 27-testing-react/src/components/Async.test.js
@@ -3,6 +3,11 @@ import Async from "./Async";
 
 describe("<Async />", () => {
   test("should render posts if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "post 1" }],
+    });
+
     render(<Async />);
 
     const listItemElements = await screen.findAllByRole("listitem");
