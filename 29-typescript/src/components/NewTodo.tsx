e7c3050fb1ebacf69aaa0d197292132879dfd884
Tomás Batista <34144094+tomas99batista@users.noreply.github.com>
2023-11-13 21:21:12 +0000

    class(611)

---
+++ 29-typescript/src/components/NewTodo.tsx
@@ -0,0 +1,15 @@
+import { FormEvent } from "react";
+
+export const NewTodo = () => {
+  const submitHandler = (event: FormEvent) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo text</label>
+      <input type="text" id="text" />
+      <button>Add Todo</button>
+    </form>
+  );
+};
